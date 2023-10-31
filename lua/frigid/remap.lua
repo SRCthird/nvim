d5be971d7772d53ce657405d815bc002dee60c78
SRCthird <SRCthird@gmail.com>
2023-10-30 23:05:18 -0400

    More configuration

--- lua/frigid/remap.lua
+++ lua/frigid/remap.lua
@@ -1,6 +1,5 @@
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
-
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
@@ -36,3 +35,7 @@ vim.keymap.set("n", "<leader>mr", "<cmd>CellularAutomaton make_it_rain<CR>");
 vim.keymap.set("n", "<leader><leader>", function()
     vim.cmd("so")
 end)
+
+vim.keymap.set("n", "<C-p>", ":NvimTreeToggle<CR>", { silent = true })
+-- UndoTree
+vim.keymap.set("n", "<leader>u", vim.cmd.UndotreeToggle)
