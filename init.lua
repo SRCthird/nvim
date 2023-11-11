6df5bc17193270abee1a61f42ebc3545cb74003c
SRCthird <SRCthird@gmail.com>
2023-11-10 20:39:26 -0500

    Require package after setting variables

--- init.lua
+++ init.lua
@@ -1,5 +1,5 @@
-require("frigid")
-
 nvim_path = debug.getinfo(1, "S").source:sub(2)
 nvim_dir = vim.fn.fnamemodify(nvim_path, ":p:h")
 home_dir = os.getenv("USERPROFILE") or os.getenv("HOME")
+
+require("frigid")
