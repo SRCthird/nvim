09878029a646f9b9da935522247933c466bb50ef
SRCthird <SRCthird@gmail.com>
2023-11-10 20:38:55 -0500

    Reformat packer.lua

--- lua/frigid/packer.lua
+++ lua/frigid/packer.lua
@@ -3,54 +3,54 @@ vim.cmd [[packadd packer.nvim]]
 
 return require('packer').startup(function(use)
     -- Packer can manage itself
-    use {'wbthomason/packer.nvim'}
+    use { 'wbthomason/packer.nvim' }
     use {
-        'nvim-telescope/telescope.nvim', 
+        'nvim-telescope/telescope.nvim',
         tag = '0.1.4',
         requires = {
             'nvim-lua/plenary.nvim'
         },
     }
-    use {'catppuccin/nvim', as = 'catppuccin' }
+    use { 'catppuccin/nvim', as = 'catppuccin' }
     use {
         'nvim-treesitter/nvim-treesitter',
         run = ':TSUpdate'
     }
-    use {'theprimeagen/harpoon'}
-    use {'mbbill/undotree'}
-    use {'tpope/vim-fugitive'}
+    use { 'theprimeagen/harpoon' }
+    use { 'mbbill/undotree' }
+    use { 'tpope/vim-fugitive' }
     use {
         'VonHeikemen/lsp-zero.nvim',
         branch = 'v3.x',
         requires = {
             -- LSP Support
-            {'williamboman/mason.nvim'},
-            {'williamboman/mason-lspconfig.nvim'},
-            {'neovim/nvim-lspconfig'},
+            { 'williamboman/mason.nvim' },
+            { 'williamboman/mason-lspconfig.nvim' },
+            { 'neovim/nvim-lspconfig' },
             -- Autocompletion
-            {'hrsh7th/nvim-cmp'},
-            {'hrsh7th/cmp-nvim-lsp'},
-            {'L3MON4D3/LuaSnip'},
+            { 'hrsh7th/nvim-cmp' },
+            { 'hrsh7th/cmp-nvim-lsp' },
+            { 'L3MON4D3/LuaSnip' },
         },
     }
-    use {'nvim-tree/nvim-tree.lua'}
-    use {'nvim-tree/nvim-web-devicons'}
-    use {'mhinz/vim-startify'}
+    use { 'nvim-tree/nvim-tree.lua' }
+    use { 'nvim-tree/nvim-web-devicons' }
+    use { 'mhinz/vim-startify' }
     use {
         'kdheepak/lazygit.nvim',
         requires = {
             'nvim-lua/plenary.nvim',
         },
     }
-    use {'terrortylor/nvim-comment'}
+    use { 'terrortylor/nvim-comment' }
     use {
         "windwp/nvim-autopairs",
         config = {
-            function() 
-                require("nvim-autopairs").setup {} 
+            function()
+                require("nvim-autopairs").setup {}
             end
         },
     }
-    use {'lewis6991/gitsigns.nvim'}
-    use {'romgrk/barbar.nvim'}
+    use { 'lewis6991/gitsigns.nvim' }
+    use { 'romgrk/barbar.nvim' }
 end)
