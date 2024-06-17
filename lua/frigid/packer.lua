c3bae2f79039158d876dea9c2638fb41c4456b9f
Stephen Chryn <SRCthird@gmail.com>
2024-06-17 15:27:58 -0400

    Update telescope

--- lua/frigid/packer.lua
+++ lua/frigid/packer.lua
@@ -6,7 +6,7 @@ return require('packer').startup(function(use)
   use { 'wbthomason/packer.nvim' }
   use {
     'nvim-telescope/telescope.nvim',
-    tag = '0.1.4',
+    tag = '0.1.8',
     requires = {
       'nvim-lua/plenary.nvim'
     },
