ff93e8a406e3821b0a4b4e0dcc1094054778b175
Stephen Chryn <SRCthird@gmail.com>
2024-06-18 18:42:54 -0400

    Open NvimTree when a file isn't called directly

--- lua/frigid/init.lua
+++ lua/frigid/init.lua
@@ -2,3 +2,7 @@ require("frigid.packer")
 require("frigid.options")
 require("frigid.remap")
 require("frigid.theme")
+
+if vim.fn.argc() == 0 then
+  vim.api.nvim_create_autocmd("VimEnter", { pattern = "*", command = ":NvimTreeToggle" })
+end
