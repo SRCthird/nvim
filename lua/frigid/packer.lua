d5462af30d767a144cdb55a97dcc64a774abad3c
Stephen Chryn <SRCthird@gmail.com>
2024-06-25 14:26:40 -0400

    Change formatting

--- lua/frigid/packer.lua
+++ lua/frigid/packer.lua
@@ -3,87 +3,88 @@ vim.cmd [[packadd packer.nvim]]
 
 return require('packer').startup(function(use)
   -- Packer can manage itself
-  use { 'wbthomason/packer.nvim' }
-  use {
+  use({ 'wbthomason/packer.nvim' })
+  use({
     'VonHeikemen/lsp-zero.nvim',
     branch = 'v3.x',
     config = function()
       require('frigid.config.lspzero')
     end
-  }
-  use {
+  })
+  use({
     'nvim-telescope/telescope.nvim',
     tag = '0.1.8',
     requires = {
       'nvim-lua/plenary.nvim'
     },
-  }
-  use {
-    'catppuccin/nvim', as = 'catppuccin',
+  })
+  use({
+    'catppuccin/nvim',
+    as = 'catppuccin',
     config = function()
       require('frigid.config.catppuccin')
     end
-  }
-  use {
+  })
+  use({
     'nvim-treesitter/nvim-treesitter',
     run = ':TSUpdate'
-  }
-  use { 'theprimeagen/harpoon' }
-  use { 'tpope/vim-fugitive' }
-  use {
+  })
+  use({ 'theprimeagen/harpoon' })
+  use({ 'tpope/vim-fugitive' })
+  use({
     'williamboman/mason.nvim',
     config = function()
       require('frigid.config.mason')
     end
-  }
-  use {
+  })
+  use({
     'williamboman/mason-lspconfig.nvim',
     config = function()
       require('mason').setup({})
       require('frigid.config.mason-lspconfig')
     end
-  }
-  use {
+  })
+  use({
     'neovim/nvim-lspconfig',
     config = function()
       require('frigid.config.lspconfig')
     end
-  }
-  use {
+  })
+  use({
     'hrsh7th/nvim-cmp',
     config = function()
       require('frigid.config.cmp')
     end
-  }
-  use { 'hrsh7th/cmp-nvim-lsp' }
-  use { 'L3MON4D3/LuaSnip' }
-  use { 'prisma/vim-prisma' }
-  use { 'chr4/nginx.vim' }
-  use {
+  })
+  use({ 'hrsh7th/cmp-nvim-lsp' })
+  use({ 'L3MON4D3/LuaSnip' })
+  use({ 'prisma/vim-prisma' })
+  use({ 'chr4/nginx.vim' })
+  use({
     'nvim-tree/nvim-tree.lua',
     config = function()
       return require('frigid.config.nvimtree')
     end,
-  }
-  use { 'nvim-tree/nvim-web-devicons' }
-  use {
+  })
+  use({ 'nvim-tree/nvim-web-devicons' })
+  use({
     'kdheepak/lazygit.nvim',
     requires = {
       'nvim-lua/plenary.nvim',
     },
-  }
-  use {
+  })
+  use({
     'windwp/nvim-autopairs',
     config = function()
-      require('nvim-autopairs').setup {}
+      require('nvim-autopairs').setup({})
     end
-  }
-  use { 'lewis6991/gitsigns.nvim' }
-  use { 'lukas-reineke/indent-blankline.nvim' }
-  use {
+  })
+  use({ 'lewis6991/gitsigns.nvim' })
+  use({ 'lukas-reineke/indent-blankline.nvim' })
+  use({
     'SRCthird/minintro.nvim',
     config = function()
       require('frigid.config.minintro')
     end
-  }
+  })
 end)
