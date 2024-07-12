e4f14140f10a5446da0540265cc0402f2c136f5b
Stephen Chryn <SRCthird@gmail.com>
2024-07-12 14:51:11 -0400

    Add SystemOS dynamic variable

--- init.lua
+++ init.lua
@@ -1,5 +1,15 @@
 NvimPath = debug.getinfo(1, "S").source:sub(2)
 NvimDir = vim.fn.fnamemodify(NvimPath, ":p:h")
 HomeDir = os.getenv("USERPROFILE") or os.getenv("HOME")
+SystemOS = (function()
+  local BinaryFormat = package.cpath:match("%p[\\|/]?%p(%a+)")
+  if BinaryFormat == "dll" or BinaryFormat == "Users" then
+    return "Windows"
+  elseif BinaryFormat == "so" then
+    return "Linux"
+  elseif BinaryFormat == "dylib" then
+    return "MacOS"
+  end
+end)()
 
 require("frigid")
