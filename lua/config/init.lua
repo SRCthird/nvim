f34eb06547ac5105af64933cb657800893fadfd3
Stephen Chryn <SRCthird@gmail.com>
2026-05-29 14:19:55 -0400

    Override netrw if argc is a path

--- lua/config/init.lua
+++ lua/config/init.lua
@@ -5,8 +5,15 @@ require("config.theme")
 
 if vim.fn.argc() == 0 then
   vim.api.nvim_create_autocmd("VimEnter", { pattern = "*", command = ":NvimTreeToggle" })
+else
+  local path = vim.fn.argv(0)
+  if vim.fn.isdirectory(path) == 1 then
+    vim.api.nvim_set_current_dir(path)
+    vim.api.nvim_create_autocmd("VimEnter", { pattern = "*", command = ":NvimTreeToggle" })
+    vim.cmd("enew")
+  end
 end
 
-vim.diagnostic.config({virtual_text = true})
+vim.diagnostic.config({ virtual_text = true })
 
 vim.cmd.colorscheme "catppuccin"
